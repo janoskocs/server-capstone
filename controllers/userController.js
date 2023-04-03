e4a40ff75fb95436b42b05d79865d0078a239f15
Janos Kocs <janos.kocs@outlook.com>
2023-04-03 19:49:40 +0100

    Create token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,24 @@
 const User = require("../models/userModel");
-//User log in
+const jwt = require("jsonwebtoken");
 
+const createToken = (_id) => {
+  return jwt.sign({ _id: _id }, process.env.SECRET, { expiresIn: "1d" });
+  //1. payload nothing sensitive, 2. secret sauce, 3.expiry
+};
+
+//User log in
 const loginUser = async (req, res) => {
   res.json({ message: "Login user" });
 };
 
 //User Sign up
-
 const signupUser = async (req, res) => {
   const { email, password } = req.body;
 
   try {
     const user = await User.signup(email, password);
-
-    res.status(200).json({ email, user });
+    const token = createToken(user._id); //Create token based on user's id
+    res.status(200).json({ email, token }); //Send email and token as response, this will be the payload, secret, and sign thingy in a hash
   } catch (error) {
     res.status(400).json({ error: error.message });
     console.log(error);
