14d4bfd9cbbd033859fac9bf68e7086ad1fe44cc
Janos Kocs <janos.kocs@outlook.com>
2023-04-04 21:12:54 +0100

    Get single user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -12,15 +12,12 @@ const getAllUsers = async (req, res) => {
 
 //Get a single user by Id
 const getSingleUser = async (req, res) => {
-  const { email, password, first_name, last_name } = req.body;
-
+  const { userId } = req.params;
   try {
-    const user = await User.signup(email, password, first_name, last_name);
-    const token = createToken(user._id); //Create token based on user's id
-    res.status(200).json({ email, token }); //Send email and token as response, this will be the payload, secret, and sign thingy in a hash
+    const user = await User.find({ _id: userId });
+    res.status(200).json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
-    console.log(error);
   }
 };
 
